ecdee53c34827077d4c77630ecdbea494288fa4c
Charles mendes <charlesmendes9@gmail.com>
2023-11-04 17:45:40 -0300

    ajustando front

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,29 @@
 <template>
-  <!-- <v-row>
-    <p>
-      What is Lorem Ipsum?
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    </p>
-    <br/>
-    <p>
-      Where does it come from?
-      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-    </p>
-  </v-row> -->
-  
+
+  <span class="title-main text-h5 ">
+    Convidados
+  </span>
+
   <div class="d-flex mb-6">
     <v-sheet class="ma-2 pa-2">
-      <span class="title-main text-h5 ">
-        Convidados
-      </span>  
+      <v-btn color="red-darken-1" flat class="elevation-0" :ripple="false" block rounded="xl" size="small" append-icon="mdi-trash-can">
+        Inativar Selecionados
+      </v-btn>
     </v-sheet>
     <v-sheet class="ma-2 pa-2">
-      <v-btn color="primary" append-icon="mdi-plus" class="elevation-1">
+      <v-btn color="primary" flat class="elevation-0" :ripple="false" block rounded="xl" size="small" append-icon="mdi-plus">
         Adicionar Convidado
       </v-btn>
     </v-sheet>
-    <v-sheet class="mt-2 ml-auto">
+    <v-sheet class="mt-2 ml-auto ">
+      <v-select
+        class="custom-search"
+        clearable
+        placeholder="Filtro"
+        :items="itemsConvidados"
+      ></v-select>
+    </v-sheet>
+    <v-sheet class="mt-2 ml-5">
       <v-text-field
         hide-details
         class="elevation-1 mx-0 px-0 custom-search"
@@ -35,12 +36,29 @@
   </div>
   
   <v-data-table
+    v-model="selected"  
     v-model:items-per-page="itemsPerPage"
     :headers="headers"
     :items="desserts"
-    item-value="name"
+    item-value="id"
     class="elevation-1"
-  ></v-data-table>
+    show-select
+  >
+    <template v-slot:item.acoes="{ item }">
+      <div class="d-flex">
+        <v-sheet class="ma-1">
+          <v-btn color="primary" flat class="elevation-0" :ripple="false" block rounded="xl" size="small" @click="editar" append-icon="mdi-pencil">
+            Editar
+          </v-btn>
+        </v-sheet>
+        <v-sheet class="ma-1">
+          <v-btn color="primary" flat class="elevation-0" :ripple="false" block rounded="xl" size="small" @click="excluir" append-icon="mdi-trash-can">
+            Excluir
+          </v-btn>
+        </v-sheet>
+      </div>
+    </template>
+  </v-data-table>
 </template>
 
 <style lang="scss">
@@ -63,36 +81,35 @@
 <script setup lang="ts">
   import { ref } from 'vue'
 
-  const itemsPerPage = ref(10)
+  const itemsPerPage = ref(10);
   const headers = ref([
-    {
-      title: 'Dessert (100g serving)',
-      align: 'start',
-      sortable: false,
-      key: 'name',
-    },
-    { title: 'Calories', align: 'end', key: 'calories' },
-    { title: 'Fat (g)', align: 'end', key: 'fat' },
-    { title: 'Carbs (g)', align: 'end', key: 'carbs' },
-    { title: 'Protein (g)', align: 'end', key: 'protein' },
-    { title: 'Iron (%)', align: 'end', key: 'iron' },
+    { title: 'Nome', align: 'start', sortable: false, key: 'nome', width: '50%'},
+    { title: 'Cargo', align: 'start', sortable: false, key: 'cargo', width: '30%' },
+    { title: 'Status', align: 'start', sortable: false, key: 'status', width: '10%' },
+    { title: 'Ações', align: 'start', sortable: false, key: 'acoes', width: '10%' }
   ])
   const desserts = ref([
     {
-      name: 'Frozen Yogurt',
-      calories: 159,
-      fat: 6.0,
-      carbs: 24,
-      protein: 4.0,
-      iron: '1',
+      id: '1',
+      nome: 'Charles Juan da Silva Mendes',
+      cargo: 'Desenvolvedor',
+      status: 'Ativo'
     },
     {
-      name: 'Jelly bean',
-      calories: 375,
-      fat: 0.0,
-      carbs: 94,
-      protein: 0.0,
-      iron: '0',
+      id: '2',
+      nome: 'Jose Carlos',
+      cargo: 'Deputador PLDB',
+      status: 'Ativo'
     },
+
   ])
+
+  const itemsConvidados = ref([
+    'Ativo', 'Inativo'
+  ])
+
+  const selected = ref([])
+
+  const editar = () => (console.log('editar'))
+  const excluir = () => (console.log('editar'))
 </script>
